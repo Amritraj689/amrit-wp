7af2c2819ca1e02a2aa11263dee4c6fe4334ec56
Amritraj689 <96330837+Amritraj689@users.noreply.github.com>
2022-07-03 21:45:54 +0530

    Delete scripts directory

--- scripts/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod -R 655 /var/www/wordpress
-systemctl restart nginx
--- scripts/validate_service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-isExistApp=`pgrep nginx`
-if [[ -n  \$isExistApp ]]; then
-   echo "nginx is running!"
-else
-   echo "nginx is not running!"
-   exit -1
-fi
